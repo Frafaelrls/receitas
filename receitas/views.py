de6737e5f2536d71561d3d0454b3ec7ad12ed0d6
Frafaelrls <frafaelrls@gmail.com>
2022-08-04 16:49:11 -0300

    Adicionando uso do modelos para a renderização das receitas

--- receitas/views.py
+++ receitas/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
+from .models import Receita
 
 def index(request):  # Verificar como que é utilizado, no método indica o uso de um parâmetro o rquest, mas seu uso não adiciona o parâmetro aparente menete, no arquivo url.py
     
-    receita = {
-        1:'Lasanha',
-        2:'Sopa de legumes',
-        3:'Sorvete',
-        4:'Bolo de chocolate'
-    }
+    receita = Receita.objects.all()
     
     dados = {
-        'nome_das_receitas' : receita
+        'receitas' : receita
     }
 
     return render(request, 'index.html', dados)
