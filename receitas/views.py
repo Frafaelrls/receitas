63805db3aa1115316f5fe773021d5c577b6e37d1
Frafaelrls <frafaelrls@gmail.com>
2022-08-04 17:18:04 -0300

    refatoração do código adicionando o reconhecimento das receitas salvas no banco de dados

--- receitas/views.py
+++ receitas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404, render
 from .models import Receita
 
 def index(request):  # Verificar como que é utilizado, no método indica o uso de um parâmetro o rquest, mas seu uso não adiciona o parâmetro aparente menete, no arquivo url.py
@@ -11,5 +11,11 @@ def index(request):  # Verificar como que é utilizado, no método indica o uso 
 
     return render(request, 'index.html', dados)
 
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_a_exibir = {
+        'receita':receita
+    }
+
+    return render(request, 'receita.html', receita_a_exibir)
