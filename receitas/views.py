55a7e74d033776be9e73bcc5f1f940d4f147d677
Frafaelrls <frafaelrls@gmail.com>
2022-08-04 11:54:05 -0300

    Adicionando dicionários para criação dimâmica de páginas

--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,19 @@ import re
 from django.shortcuts import render
 
 def index(request):  # Verificar como que é utilizado, no método indica o uso de um parâmetro o rquest, mas seu uso não adiciona o parâmetro aparente menete, no arquivo url.py
-    return render(request, 'index.html')
+    
+    receita = {
+        1:'Lasanha',
+        2:'Sopa de legumes',
+        3:'Sorvete',
+        4: 'Bolo de chocolate'
+    }
+    
+    dados = {
+        'nome_das_receitas' : receita
+    }
+
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
