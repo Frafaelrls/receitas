3987a75eace4fad6f0440be7dd297c10386d38a0
Frafaelrls <frafaelrls@gmail.com>
2022-08-11 11:00:13 -0300

    Adicionando o uso da nomenclatura para as receitas no painel de edição do admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Receita
 
-admin.site.register(Receita)
+# Classe para definir a nomenclatura para as receitas criadas
+class ListandoReceitas(admin.ModelAdmin):
+    # Tupla de quais colunas devem aparecer no editar receitas
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo' )
+    # Tupla de quais colunas possuirão links
+    list_display_links = ('id','nome_receita')
+
+admin.site.register(Receita, ListandoReceitas)
