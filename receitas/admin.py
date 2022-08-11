ead418f5dbb963ae93b8f5f4b7a1da17f542cfd3
Frafaelrls <frafaelrls@gmail.com>
2022-08-11 11:14:56 -0300

    Adicionando filtros no modo admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 
 from .models import Receita
 
-# Classe para definir a nomenclatura para as receitas criadas
+# Classe para manupular a exibição no modo adimin das receitas criadas
 class ListandoReceitas(admin.ModelAdmin):
     # Tupla de quais colunas devem aparecer no editar receitas
-    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo' )
+    list_display: list|tuple = ('id', 'nome_receita', 'categoria', 'tempo_preparo' )
     # Tupla de quais colunas possuirão links
-    list_display_links = ('id','nome_receita')
+    list_display_links: list|tuple = ('id','nome_receita')
+    # Campo de busca
+    search_fields: list|tuple = ('nome_receita',)
+    # Limite por página
+    list_per_page: int = 2
+    # Filtro
+    list_filter: list|tuple = ('categoria',)
 
 admin.site.register(Receita, ListandoReceitas)
