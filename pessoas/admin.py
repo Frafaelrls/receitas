f2463da181619e15dd23f4660fb7140c6e31dfef
Frafaelrls <frafaelrls@gmail.com>
2022-08-19 10:31:29 -0300

    Adicionando método mágico para exibição do nome do objeto pessoa

--- pessoas/admin.py
+++ pessoas/admin.py
@@ -5,6 +5,6 @@ class ListandoPessoas(admin.ModelAdmin):
     list_display = ('id', 'nome', 'email')
     list_display_links = ('id', 'nome')
     search_fields = ('nome',)
-    list_per_page: int = 2
+    list_per_page = 2
 
 admin.site.register(Pessoa, ListandoPessoas)
