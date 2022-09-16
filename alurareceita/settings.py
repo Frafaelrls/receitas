364247b465fb74c516289c86d14531bb4cf46828
Frafaelrls <frafaelrls@gmail.com>
2022-09-16 11:14:01 -0300

    adicionando caminho para a pasta app que será criada

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
+import os, sys
 from pathlib import Path
 from .database import PSW
 
@@ -141,3 +141,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     messages.SUCCESS: 'success',
 }
+
+
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, '../apps'))
